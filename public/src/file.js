1b0a8f82297ef7176d5ea7ac67d06af64f94d783
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-11-19 15:55:34 +0000

    Add files via upload

--- public/src/file.js
+++ public/src/file.js
@@ -1,8 +1,29 @@
-  const displayInfo = () => {
-    const xhttp = new XMLHttpRequest();
-  xhttp.onload = function() {
-    document.getElementById("demo").innerHTML = this.responseText;
+//   const displayInfo = () => {
+//     const xhttp = new XMLHttpRequest();
+//   xhttp.onload = function() {
+//     document.getElementById("demo").innerHTML = this.responseText;
+//   }
+//   xhttp.open("GET", '/M00915023/users');
+//   xhttp.send();
+// }
+const sexInput = document.querySelector("#sex");
+
+
+document.getElementById("demo").addEventListener("click", async () => {
+
+  const userSex = sexInput.value;
+
+  try{
+    const response = await fetch(`/M00915023/users/${userSex}`);
+    if (!response.ok) {
+      throw new Error("Network response failed");
+    }
+
+    const data = await response.json();
+
+    document.getElementById('dataDisplay').innerHTML = JSON.stringify(data);
+
+  } catch (err) {
+    console.log(err)
   }
-  xhttp.open("GET", '/M00915023/users');
-  xhttp.send();
-}
+})
